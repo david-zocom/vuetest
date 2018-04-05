0613fddd496bfd060fcb825d2e3d28d30ca5423c
David Andersson <lejonmanen@gmail.com>
2018-04-05 17:14:07 +0200

    initial

---
+++ src/components/Comp1.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="comp1">
+		Hello! This is component 1.
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+.comp1 {
+	border: 2px solid brown;
+	border-radius: 4px;
+	padding: 10px;
+	margin: 10px;
+	text-align: center;
+}
+</style>
---
+++ src/components/Comp2.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		{{animal}}
+		<button v-on:click="$emit('party-event')">Click me</button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['animal']
+}
+</script>
+
+
+<style scoped>
+div {
+	background-color: pink;
+}
+</style>
